97fbe5b0803956db207231cc24c2d1d750229bc5
ReeInk <hi@ree.ink>
2023-07-19 13:27:32 +0800

    work7: update pyramid image

--- image_pyramid.py
+++ image_pyramid.py
@@ -1,9 +1,8 @@
 import os
-from typing import Iterable, Callable
+from typing import Iterable, Callable, List
 import cv2
 import numpy as np
 from rich.progress import track
-from rich import print
 from downsample import downsample_from_data as downsample
 from upsample import (
     nearest_interpolation,
@@ -65,14 +64,36 @@ def pyramid_builder(
     while image.shape[0] > 1 and image.shape[1] > 1:
         next_level = compose(approximation_filter, downsample)(image)
         next_level = next_level.reshape(next_level.shape[0], next_level.shape[1], -1)
-        now_level = image - compose(upsample, interpolaion_filter)(next_level).reshape(image.shape)
+        now_level = image - compose(upsample, interpolaion_filter)(next_level).reshape(
+            image.shape
+        )
         yield now_level
         image = next_level
     yield image
 
+
+def merge_image(images: List[np.ndarray]) -> np.ndarray:
+    if len(images[0].shape) == 3:
+        channels = images[0].shape[2]
+    elif len(images[0].shape) == 2:
+        channels = 1
+    else:
+        raise ValueError("Image shape error")
+
+    size = images[0].shape[0]
+    ret_image = np.ones((size, size * 2, channels)) * 255
+    start_x = 0
+    for image in images:
+        image_size = image.shape[0]
+        ret_image[0:image_size, start_x : start_x + image_size, :] = image
+        start_x += image_size
+
+    return ret_image
+
+
 if __name__ == "__main__":
     SOURCE_PATH = "images/512"
-    
+
     # DIST_PATH = "images/512_pyramid"
 
     # approximation_filters = {
@@ -86,29 +107,44 @@ if __name__ == "__main__":
     #     "bilinear": lambda x: bilinear_interpolation(x, x.shape[0] * 2, x.shape[1] * 2),
     #     "bicubic": lambda x: bicubic_interpolation(x, x.shape[0] * 2, x.shape[1] * 2),
     # }
-    
+
     DIST_PATH = "images/512_pyramid_opencv"
-    
+
     approximation_filters = {
         "mean": lambda x: cv2.blur(x, (3, 3)),
         "gaussian": lambda x: cv2.GaussianBlur(x, (3, 3), 1),
         "subsampling": lambda x: x,
     }
-    
+
     interpolation_filters = {
-        "nearest": lambda x: cv2.resize(x, (x.shape[0] * 2, x.shape[1] * 2), interpolation=cv2.INTER_NEAREST),
-        "bilinear": lambda x: cv2.resize(x, (x.shape[0] * 2, x.shape[1] * 2), interpolation=cv2.INTER_LINEAR),
-        "bicubic": lambda x: cv2.resize(x, (x.shape[0] * 2, x.shape[1] * 2), interpolation=cv2.INTER_CUBIC),
+        "nearest": lambda x: cv2.resize(
+            x, (x.shape[0] * 2, x.shape[1] * 2), interpolation=cv2.INTER_NEAREST
+        ),
+        "bilinear": lambda x: cv2.resize(
+            x, (x.shape[0] * 2, x.shape[1] * 2), interpolation=cv2.INTER_LINEAR
+        ),
+        "bicubic": lambda x: cv2.resize(
+            x, (x.shape[0] * 2, x.shape[1] * 2), interpolation=cv2.INTER_CUBIC
+        ),
     }
 
     for apf_name, apf in approximation_filters.items():
         for inf_name, inf in interpolation_filters.items():
             dist_path = os.path.join(DIST_PATH, f"{apf_name}_{inf_name}")
             os.makedirs(dist_path, exist_ok=True)
-            for filename in track(os.listdir(SOURCE_PATH), description=f"{apf_name}_{inf_name}"):
-                image = cv2.imread(os.path.join(SOURCE_PATH, filename), cv2.IMREAD_GRAYSCALE)
-                pyramid = reversed(list(pyramid_builder(image, apf, inf)))
-                for level, image in enumerate(pyramid):
+            for filename in track(
+                os.listdir(SOURCE_PATH), description=f"{apf_name}_{inf_name}"
+            ):
+                image = cv2.imread(
+                    os.path.join(SOURCE_PATH, filename), cv2.IMREAD_GRAYSCALE
+                )
+                pyramid = list(pyramid_builder(image, apf, inf))
+                pyramid_images = merge_image(pyramid)
+                cv2.imwrite(
+                    os.path.join(dist_path, f"{filename.split('.')[0]}_pyramid.jpg"),
+                    pyramid_images,
+                )
+                for level, image in enumerate(reversed(pyramid)):
                     cv2.imwrite(
                         os.path.join(
                             dist_path, f"{filename.split('.')[0]}_l{level+1}.bmp"
